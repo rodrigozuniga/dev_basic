2c1ea50820cb4be663635c93348990c983156bd8
rodrigo-zuniga <rodrigo.zuniga@airbnb.com>
2022-06-13 12:12:19 -0700

    test for file

--- bash_test.sh
+++ bash_test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 if [ -d /usr/share/wordlists ]
-then echo 'it does exists'
-else echo 'it does NOT exists'
+then echo 'dir does exists'
+else echo 'dir does NOT exists'
+fi
+
+if [ -e /Users/rodrigo_zuniga/test ]
+then echo 'file does exists'
+else echo 'file does not exist' 
 fi
